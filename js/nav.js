77073270287c6e641d7fd09784c2ef929a4af9c6
josebenitoge <68750927+josebenitoge@users.noreply.github.com>
2022-03-08 20:09:13 +0100

    nav.js, na.css, full

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,10 @@
 window.onscroll = function () {
+  let nav = document.getElementById("nav");
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("nav").style.boxShadow =
-      "0px 0px 20px rgba(0, 0, 0, 0.4)";
+    nav.style.boxShadow = "0px 0px 20px rgba(0, 0, 0, 0.4)";
+    nav.style.backgroundColor = "var(--bg2)";
   } else {
-    document.getElementById("nav").style.boxShadow =
-      "0px 1px 0px var(--border)";
+    nav.style.boxShadow = "0px 1px 0px var(--border)";
+    nav.style.backgroundColor = "transparent";
   }
 };
