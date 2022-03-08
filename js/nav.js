a2cd844bdc7d6d6212dcb3b6fe98371e40133f37
josebenitoge <68750927+josebenitoge@users.noreply.github.com>
2022-03-08 19:24:13 +0100

    nav

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 window.onscroll = function () {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("nav").style.boxShadow =
-      "0px 0px 7px rgba(0, 0, 0, 0.4)";
+      "0px 0px 20px rgba(0, 0, 0, 0.4)";
   } else {
     document.getElementById("nav").style.boxShadow =
       "0px 1px 0px var(--border)";
